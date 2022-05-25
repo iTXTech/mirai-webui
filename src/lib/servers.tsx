334faa22316878ff02a6d2d510f43b6f6aa14c6a
jasonczc <jasonczc@qq.com>
2022-05-25 23:12:05 +0800

    dd localstorage

--- src/lib/servers.tsx
+++ src/lib/servers.tsx
@@ -1,6 +1,54 @@
+import {randomUUID} from "crypto";
+
 export interface SoyuzConnectionInfo {
+    id:string
     name:string
     ip:string
     port:string
     token:string
+    addTime:Date
+}
+
+const KEY_SOYUZ_CONNECTIONS = "soyuz_connections"
+
+export function getConnectionsFromStorage():Array<SoyuzConnectionInfo> {
+    const res = window.localStorage.getItem(KEY_SOYUZ_CONNECTIONS)
+    if(res==="" || !res) return []
+    else {
+        try {
+            const arr = JSON.parse(res as string)
+            if(!(arr instanceof Array)) throw new Error()
+            return arr
+        }catch (e) {
+            return []
+        }
+    }
+}
+
+export function setConnectionsToLocalStorage(arr:Array<SoyuzConnectionInfo>) {
+    const res = JSON.stringify(arr)
+    window.localStorage.setItem(KEY_SOYUZ_CONNECTIONS, res)
+}
+
+export function addSoyuzConnection(conn:SoyuzConnectionInfo) {
+    const arr = getConnectionsFromStorage()
+    if(conn.id===""||!conn.id) {
+        conn.id = randomUUID()
+    }
+    arr.push(conn)
+    setConnectionsToLocalStorage(arr)
+}
+
+export function deleteSoyuzConnectionBy(predicate: (value: SoyuzConnectionInfo, index: number, array: SoyuzConnectionInfo[]) => unknown) {
+    const arr = getConnectionsFromStorage()
+    arr.filter(predicate)
+    setConnectionsToLocalStorage(arr)
+}
+
+export function deleteSoyuzConnectionByName(connName: string) {
+    deleteSoyuzConnectionBy((val)=>val.name!==connName)
+}
+
+export function deleteSoyuzConnectionByID(ID: string) {
+    deleteSoyuzConnectionBy((val)=>val.id!==ID)
 }
